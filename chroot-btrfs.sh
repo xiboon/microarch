3250a41ee8de5dc441997616174d75b756e2817a
xiboon <38988733+xiboon@users.noreply.github.com>
2022-01-16 16:44:22 +0100

    fuck me

--- chroot-btrfs.sh
+++ chroot-btrfs.sh
@@ -3,7 +3,7 @@
 echo "MODULES=(btrfs) \
 BINARIES=() \
 FILES=() \
-HOOKS=(base udev autodetect modconf block filesystems keyboard keymap encrypt lvm2 filesystems fsck)" > /etc/mkinitcpio.conf
+HOOKS=(base udev autodetect modconf block filesystems fsck)" > /etc/mkinitcpio.conf
 pacman --noconfirm -S grub
 grub-install --target=i386-pc --force /dev/vda
 grub-mkconfig -o /boot/grub/grub.cfg
