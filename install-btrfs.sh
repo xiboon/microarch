5aacfefd6288e7c9b5eef3b17e7ef3dc931134ff
xiboon <38988733+xiboon@users.noreply.github.com>
2022-01-16 16:19:12 +0100

    yup

--- install-btrfs.sh
+++ install-btrfs.sh
@@ -6,20 +6,21 @@ then
     echo "This script is not compatible with UEFI!"
 else
     # create partitions
-    parted /dev/vda \ mklabel msdos \ mkpart primary 1 512M \ mkpart primary 512M 100% -s
+    parted $DEV \ mklabel msdos \ mkpart primary 1 512M \ mkpart primary 512M 100% -s
     # format partitions
-    mkfs.fat -F32 /dev/vda1
-    mkfs.btrfs /dev/vda2
+    mkfs.fat -F32 $DEV1
+    mkfs.btrfs $DEV2
     # mount fs
-    mount -o compress=zstd /dev/vda2 /mnt
+    mount -o compress=zstd $DEV2 /mnt
     mkdir /mnt/boot
-    mount /dev/vda1 /mnt/boot
+    mount $DEV1 /mnt/boot
     # install needed packages
     pacstrap /mnt linux-hardened pacman dhcpcd sed nano systemd-sysvcompat pam btrfs-progs
     echo $HOSTNAME >> /mnt/etc/hostname
     # generate a fstab and go to chroot
     genfstab -U /mnt >> /mnt/etc/fstab
     chmod +x chroot-btrfs.sh
+    cp chroot-btrfs.sh /mnt
     arch-chroot /mnt ./chroot-btrfs.sh
     echo "Finished! You can now reboot, if there are any issues please report them on github."
 fi
