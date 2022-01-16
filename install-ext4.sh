e1aaf12997cd873057e543a5aaec1e25e1e6138e
xiboon <38988733+xiboon@users.noreply.github.com>
2022-01-16 17:54:19 +0100

    probably ext4

--- install-ext4.sh
+++ install-ext4.sh
@@ -9,24 +9,20 @@ else
     parted ${DEV} \ mklabel msdos \ mkpart primary 1 512M \ mkpart primary 512M 100% -s
     # format partitions
     mkfs.fat -F32 ${DEV}1
-    mkfs.btrfs -f ${DEV}2
+    mkfs.ext4 ${DEV}2
     # mount fs
-    mount -o compress=zstd ${DEV}2 /mnt
+    mount ${DEV}2 /mnt
     mkdir /mnt/boot
     mkdir /mnt/etc
     mount ${DEV}1 /mnt/boot
     # mkinitcpio
-    echo "MODULES=(btrfs) \
-BINARIES=() \
-FILES=() \
-HOOKS=(base udev autodetect modconf block filesystems fsck)" > /mnt/etc/mkinitcpio.conf
     # install needed packages
-    pacstrap /mnt linux-hardened pacman dhcpcd sed nano systemd-sysvcompat pam btrfs-progs
+    pacstrap /mnt linux-hardened pacman dhcpcd sed nano systemd-sysvcompat pam
     echo $HOSTNAME >> /mnt/etc/hostname
     # generate a fstab and go to chroot
     genfstab -U /mnt >> /mnt/etc/fstab
-    chmod +x chroot-btrfs.sh
-    cp chroot-btrfs.sh /mnt
+    chmod +x chroot.sh
+    cp chroot.sh /mnt
     arch-chroot /mnt ./chroot.sh
     echo "Finished! You can now reboot, if there are any issues please report them on github."
 fi
