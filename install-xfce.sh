da321c72912bfeb0ba8867ce669a4f389d0c0551
xiboon <38988733+xiboon@users.noreply.github.com>
2022-01-16 21:06:41 +0100

    speed

--- install-xfce.sh
+++ install-xfce.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cp xfce-chroot.sh /mnt
 arch-chroot /mnt ./xfce-chroot.sh
+rm -rf /mnt/var/cache
+cd /mnt/usr/share
+rm -rf man doc gtk-doc
 echo "Installed xfce4 with lxdm!"
 echo "You can now reboot, if there were any issues make sure you ran the script from the live cd after installation or report them on github."
